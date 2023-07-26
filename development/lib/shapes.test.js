61295668e58e4678bb3a2d02c234541d52b53f62
CSillus <71670415+csills1416@users.noreply.github.com>
2023-07-26 06:35:35 -0400

    created shape.js and test. Created framework js for test

---
+++ development/lib/shapes.test.js
@@ -0,0 +1,17 @@
+const { Triangle, Square, Circle } = require('./shapes');
+
+describe ('Triangle', () => {
+    it ('should create a triangle', () => {
+        // Write a test for Triangle class
+        }));
+describe ('Square', () => {
+    it ('should create a square', () => {
+        // Write a test for Square class
+        }));
+        
+describe ('Circle', () => {
+    it ('should create a circle', () => {
+        // Write a test for Circle class
+        }));
+
+// Note to self: We should go back to this. Need to study more.
