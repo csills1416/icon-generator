675f3bd9226aa8d923477db70824a7a62e83d995
CSillus <71670415+csills1416@users.noreply.github.com>
2023-07-27 06:01:03 -0400

    added S, C, T shapes to shapes.js

--- development/lib/shapes.js
+++ development/lib/shapes.js
@@ -1,6 +1,40 @@
-class Triangle {}
-class Square {}
+class Triangle {
+    static generateSVG(color) {
+      const sideLength = 100;
+      const height = (Math.sqrt(3) / 2) * sideLength;
+      const centerX = 150;
+      const centerY = 100;
+  
+      const points = `${centerX},${centerY - height / 2} ${centerX - sideLength / 2},${centerY + height / 2} ${centerX + sideLength / 2},${centerY + height / 2}`;
+  
+      return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="${points}" fill="${color}" />
+      </svg>`;
+    }
+  }
+  
+class Square {
+    static generateSVG(color) {
+      const sideLength = 100;
+      const x = 150 - sideLength / 2;
+      const y = 100 - sideLength / 2;
+  
+      return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect x="${x}" y="${y}" width="${sideLength}" height="${sideLength}" fill="${color}" />
+      </svg>`;
+    }
+  }
+  
 
-class Circle {}
-
-module.elxports = {Triangle, Square, Circle};
+class Circle {
+    static generateSVG(color) {
+      const centerX = 150;
+      const centerY = 100;
+      const radius = 50;
+  
+      return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="${centerX}" cy="${centerY}" r="${radius}" fill="${color}" />
+      </svg>`;
+    }
+  }
+  
