cde5229693214b053c9982020d0e2735bfe1163f
CSillus <71670415+csills1416@users.noreply.github.com>
2023-07-27 18:07:21 -0400

    Wrote test code for shapes.test.js

--- development/index.js
+++ development/index.js
@@ -1,32 +1,62 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Triangle, Square, Circle } = require('./shapes.js'); 
+const { Triangle, Square, Circle } = require('./shapes.js');
 
-function generateSVG(shape, shapeColor, text, textColor) {
-    try {
-        const {text, textColor, shapeColor, shape} = await inquirer.prompt ({
-            type: 'input',
-            name: 'text',
-            message: 'Enter the text for the shape:',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter the text color for the shape:',
-        },
-        {
-            type: 'input',
-            name:'shapeColor',
-            message: 'Enter the color for the shape:',
-        },
-    });
+async function generateSVG(shape, shapeColor, text, textColor) {
+  let svg;
+  try {
+    switch (shape) {
+      case 'triangle':
+        svg = Triangle.generateSVG(shapeColor, text, textColor);
+        break;
+      case 'square':
+        svg = Square.generateSVG(shapeColor, text, textColor);
+        break;
+      case 'circle':
+        svg = Circle.generateSVG(shapeColor, text, textColor);
+        break;
+      default:
+        throw new Error('Invalid shape selected');
+    }
 
-    const svg = generateSVG(shapeColor, text, textColor, shape);
     fs.writeFileSync('logo.svg', svg);
     console.log('Your SVG has been generated!');
     return svg;
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-    promptUser();
+async function promptUser() {
+  try {
+    const { text, textColor, shape, shapeColor } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter the text for the shape:',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color for the shape:',
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the color for the shape:',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+    ]);
+
+    await generateSVG(shape, shapeColor, text, textColor);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+promptUser();
--- development/lib/shapes.test.js
+++ development/lib/shapes.test.js
@@ -1,17 +1,25 @@
 const { Triangle, Square, Circle } = require('./shapes');
 
-describe ('Triangle', () => {
-    it ('should create a triangle', () => {
-        // Write a test for Triangle class
-        }));
-describe ('Square', () => {
-    it ('should create a square', () => {
-        // Write a test for Square class
-        }));
-        
-describe ('Circle', () => {
-    it ('should create a circle', () => {
-        // Write a test for Circle class
-        }));
+describe('Triangle', () => {
+  it('should create a triangle', () => {
+    const color = 'red';
+    const triangle = new Triangle(color);
+    expect(triangle.color).toBe(color);
+  });
+});
 
-// Note to self: We should go back to this. Need to study more.
+describe('Square', () => {
+  it('should create a square', () => {
+    const color = 'blue';
+    const square = new Square(color);
+    expect(square.color).toBe(color);
+  });
+});
+
+describe('Circle', () => {
+  it('should create a circle', () => {
+    const color = 'green';
+    const circle = new Circle(color);
+    expect(circle.color).toBe(color);
+  });
+});
